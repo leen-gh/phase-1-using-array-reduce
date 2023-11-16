02327ac3315d22d1193f68b16bf404998b98837a
leen-gh <leen.ghaleb.d@gmail.com>
2023-11-17 00:33:14 +0300

    assigment is done

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+    let totalBatteries = batteryBatches.reduce((total, current)=>{
+        return total +current
+    },0)
+
+
+
 // Code your solution here
